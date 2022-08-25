0f7c34dafdc22654e99a893f94e4541cfb9a2a69
billytrinh <quanghoa.trinh@gmail.com>
2022-08-25 11:37:13 +0700

    up code

--- index.js
+++ index.js
@@ -6,9 +6,34 @@ app.listen(port,function () {
     console.log("Server is running...");
 })
 
+const mysql = require("mysql");
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root", // xampp: "", mamp: "root"
+    database: "T2204M",
+    port: 3306
+});
+
 app.get("/",function (req,res) {
     res.send("Hello world!");
 })
 app.get("/bong-da",function (req,res) {
     res.send("Bóng đá 24h");
-})
+});
+// api danh sach category
+app.get("/api-get-category",function (req,res) {
+    const sql_txt = "select * from category";
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("Error");
+        else res.send(data);
+    })
+});
+// api ds product with join
+app.get("/api-get-product",function (req,res) {
+    const sql_txt = "select product.id,product.name,product.price,category.name as category_name from product left join category on product.categoryId = category.id;";
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("Error");
+        else res.send(data);
+    })
+});
