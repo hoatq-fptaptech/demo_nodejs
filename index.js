55a55e6932ac0be785d70b4de040dbbf33fadccd
billytrinh <quanghoa.trinh@gmail.com>
2022-08-27 08:37:27 +0700

    up code filter & search

--- index.js
+++ index.js
@@ -37,3 +37,32 @@ app.get("/api-get-product",function (req,res) {
         else res.send(data);
     })
 });
+app.get("/api-product-by-category",function (req,res) {
+    const categoryId = req.query.categoryId;
+    const sql_txt = "select product.id,product.name,product.price,category.name as " +
+        "category_name from product left join category on product.categoryId = " +
+        "category.id where categoryId = "+categoryId;
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("Error");
+        else res.send(data);
+    })
+});
+app.get("/search-product",function (req,res) {
+    const q = req.query.q;
+    const sql_txt = `select * from product where name like '%${q}%'`;
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("Error");
+        else res.send(data);
+    })
+})
+app.get("/detail-product",function (req,res) {
+    const id = req.query.id;
+    const sql_txt = "select * from product where id = "+id;
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("Error");
+        else if(data.length ==0)
+                res.send("404 not found");
+        else
+            res.send(data[0]);
+    })
+})
